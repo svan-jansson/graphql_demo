a0aed38677150977a4c9981a76d0e4a2d6c313f3
Svan Jansson <svan.jansson@gmail.com>
2021-05-14 15:59:11 +0200

    Get stories by author

--- src/Queries/NewsfeedQuery.cs
+++ src/Queries/NewsfeedQuery.cs
@@ -1,5 +1,6 @@
 ﻿using GraphQl.Demo.NewsfeedData;
 using GraphQl.Demo.Types;
+using GraphQL;
 using GraphQL.Types;
 
 namespace GraphQl.Demo.Queries
@@ -19,6 +20,19 @@ namespace GraphQl.Demo.Queries
                 name: "getAuthors",
                 description: "Get the authors behind the stories in the newsfeed",
                 resolve: context => newsfeedData.GetAuthors());
+
+            Field<ListGraphType<StoryType>>(
+                name: "getStoriesByAuthor",
+                description: "Get all stories by a given author",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = "authorId",
+                        Description = "The unique ID of the author"
+                    }
+                ),
+
+                resolve: context => newsfeedData.GetStoriesByAuthor(context.GetArgument<int>("authorId")));
         }
     }
 }
